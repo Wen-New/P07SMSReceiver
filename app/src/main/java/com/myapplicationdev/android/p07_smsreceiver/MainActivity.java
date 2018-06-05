a6b3c0605a07dfc8c7d9b403bea78b1acdda7544
Wen-New <16022742@myrp.edu.sg>
2018-06-05 18:30:55 +0800

    Initial commit

---
+++ app/src/main/java/com/myapplicationdev/android/p07_smsreceiver/MainActivity.java
@@ -0,0 +1,25 @@
+package com.myapplicationdev.android.p07_smsreceiver;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.Fragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        Fragment f1 = new FragmentFirst();
+        ft.replace(R.id.frame1, f1);
+        Fragment f2 = new FragmentSecond();
+        ft.replace(R.id.frame2, f2);
+
+        ft.commit();
+    }
+}
